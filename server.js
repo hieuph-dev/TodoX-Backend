4f237e6123feae9ef8c2a0c4aba676dee5badabe
Neo Pham <hieupham250201@gmail.com>
2025-10-22 22:50:44 +0700

    prepare to deploy

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-import express, { response } from 'express'
-import taskRoutes from './src/routes/taskRoutes.js'
-import { connectDB } from './src/config/db.js'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express, { response } from "express"
+import taskRoutes from "./src/routes/taskRoutes.js"
+import { connectDB } from "./src/config/db.js"
+import dotenv from "dotenv"
+import cors from "cors"
+import path from "path"
 
 // Load from .env
 dotenv.config()
 
 // PORT
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // Create Express Application
 const app = express()
 
 // Middleware
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:5173' }))
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173" }))
+}
 
 // Init routes
-app.use('/api/tasks/', taskRoutes)
+app.use("/api/tasks/", taskRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 // Just start server when connect DB successfully
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log('server bắt đầu chạy trên cổng', PORT)
+        console.log("server bắt đầu chạy trên cổng", PORT)
     })
 })
